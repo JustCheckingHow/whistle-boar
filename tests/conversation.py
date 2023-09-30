88c692a12f6b2cea9faede487259323eb4ca5323
ConsoleRunner <wojciechly@wp.pl>
2023-10-01 00:30:31 +0200

    refactor: simplify expressions

--- tests/conversation.py
+++ tests/conversation.py
@@ -9,14 +9,15 @@ def extract_next_action(xml):
     if "Hangup" in xml:
         return "hangup"
     try:
-        return re.search(r'action="(.+?)"', xml).group(1)
+        return re.search(r'action="(.+?)"', xml)[1]
     except AttributeError:
         print(xml)
 
 def extract_speech_result(xml):
     # the speech result is given in the form of `<Say (...)>...</Say>`
-    return re.search(r'<Say.*?>(.+?)</Say>', xml).group(1)
+    return re.search(r'<Say.*?>(.+?)</Say>', xml)[1]
 
+import contextlib
 if __name__ == "__main__":
     # initialize the conversation
     text = input("Start conversation [/menu]: ")
@@ -31,12 +32,9 @@ if __name__ == "__main__":
         action = extract_next_action(r.text)
         if action == "hangup":
             break
-        try:
+        with contextlib.suppress(AttributeError):
             response = extract_speech_result(r.text)
             print(f"Assistant: {response}")
-        except AttributeError:
-            pass
-
         if "Gather" in r.text:
             # get the next text to say
             text = input(f"Say something [/{action}]: ")
@@ -45,5 +43,5 @@ if __name__ == "__main__":
             r = s.post(f"{HOST}/{action}", data={"SpeechResult": text})
         else:
             r = s.post(f"{HOST}/{action}")
-        
+
         time.sleep(0.5)
